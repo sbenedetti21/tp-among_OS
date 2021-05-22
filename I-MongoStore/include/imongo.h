29f3a9a9152130ca58bcbb568a4bd86e03cf45a5
franciscoannoni <franciscoannoni@gmail.com>
2021-05-22 09:15:42 -0300

    agrego algo

--- I-MongoStore/include/imongo.h
+++ I-MongoStore/include/imongo.h
@@ -25,6 +25,62 @@ void leerConfig(){
 
 }
 
+t_bitarray * crearBitMap(){
+size_t sizeBitMap = cantidadDeBloques / 8; 
+void * bitMap = malloc(sizeBitMap);
+
+t_bitarray * punteroBitmap =  bitarray_create(bitMap, sizeBitMap);
+
+for(int i = 0; i<cantidadDeBloques; i++){
+	  bitarray_set_bit(punteroBitmap,i);
+	  printf("%d",bitarray_test_bit(punteroBitmap,i));
+  }
+
+return punteroBitmap;
+}
+
+void crearSuperBloque(){
+//crear bloque y tamanio
+ char * ubicacionSuperBloque = string_from_format("%s/SuperBloque.ims",puntoDeMontaje);
+ FILE * superBloque; 
+ superBloque = fopen(ubicacionSuperBloque,"w");
+ fwrite(&tamanioDeBloque, sizeof(uint32_t), 1, superBloque);
+ fwrite(&cantidadDeBloques, sizeof(uint32_t), 1, superBloque);
+ 
+//crear bitmap
+ t_bitarray* bitMap = crearBitMap();
+ txt_write_in_file(superBloque, bitMap);
+ //fwrite(bitMap,sizeof(cantidadDeBloques / 8),1,superBloque);
+
+ fclose(superBloque);
+ bitarray_destroy(bitMap);
+ //-----------------
+ leerBitMap();
+ //bitarray_destroy(leerBitMap());
+}
+
+
+void * leerBitMap(){
+ char * ubicacionSuperBloque = string_from_format("%s/SuperBloque.ims",puntoDeMontaje);
+ FILE * superBloque; 
+ superBloque = fopen(ubicacionSuperBloque,"r");
+ size_t sizeBitMap = cantidadDeBloques / 8; 
+ void * bitMap = malloc(sizeBitMap);
+
+ fread(&tamanioDeBloque, sizeof(uint32_t), 1, superBloque);
+ printf("%d",tamanioDeBloque);
+ fread(&cantidadDeBloques, sizeof(uint32_t), 1, superBloque);
+ printf("%d",cantidadDeBloques);
+ fread(bitMap, sizeBitMap,1,superBloque);
+
+ t_bitarray * punteroBitmap =  bitarray_create(bitMap, sizeBitMap);
+ for(int i = 0; i<cantidadDeBloques; i++){
+	  printf("%d",bitarray_test_bit(punteroBitmap,i));
+  }
+ fclose(superBloque);
+}
+
+
 
 
 #endif
