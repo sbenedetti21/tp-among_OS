43de3f3b312a3c5e25e0c0cf0bff1e951634c0f3
Santiago Benedetti <62355123+sbenedetti21@users.noreply.github.com>
2021-07-30 23:43:55 -0300

    Update removeRepo.sh

--- removeRepo.sh
+++ removeRepo.sh
@@ -2,7 +2,13 @@
 length=$(($#-1))
 OPTIONS=${@:1:$length}
 CWD=$PWD
-echo -e "Eliminando repo"
+echo -e "\n\nEliminando repo\n\n"
 cd /home/utnso/TPCUATRI
 rm -rf tp-2021-1c-Pascusa/
 git clone https://github.com/sisoputnfrba/tp-2021-1c-Pascusa
+cd /home/utnso/TPCUATRI/tp-2021-1c-Pascusa
+echo -e "\n\nBuilding projects...\n\n"
+make -C ./Discordiador
+make -C ./I-MongoStore
+make -C ./Mi-RAM
+echo -e "\n\nFletaste el repo exitosamente FIUUUMBA\n\n"
